f644556e4124f69704c612fdf672d7cc86133c37
maciejmar <memgos@gmail.com>
2025-12-27 09:36:51 +0100

    ex12.py additional lines

--- ex_12.py
+++ ex_12.py
@@ -19,6 +19,20 @@ class Pies(Zwierze):
         return("wrrr....")
     def __str__(self)->str:
         return f"the dog {self.name} is sizeOf {self.size} and is serving {self.serving}"
+    
+class Cat(Zwierze):
+    def __init__(self, name, size, trick:str)->None:
+        self.trick=trick
+        super().__init__(name, size)    
+    def sound(self)->str:
+        return "miau"
+    def __str__(self)->str:
+        return f"the cat {self.name} size of {self.size} and knows tricks {self.trick}"
+    
+def makeZwierze(data_flush:dict)->Zwierze:
+    return 
         
 pies_1 = Pies("burek",18,"aport")
+kot_1 = Cat("mruczek",10,"ble")
 print(pies_1)
+print(kot_1)
